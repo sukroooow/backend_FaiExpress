ea81143fdf34810618e8b890c6bc4a98b7582c0c
Ridho Mubarok <dhxo.pyprogram@gmail.com>
2025-07-09 17:40:43 +0700

    addding some controller

--- model/order.go
+++ model/order.go
@@ -7,12 +7,15 @@ import (
 )
 
 type Order struct {
-	ID           uint           `gorm:"primaryKey" json:"id"`
-	CustomerID   uint           `json:"customer_id"`
-	KurirID      uint           `json:"kurir_id"` // bisa null, makanya pointer
+	ID         uint `gorm:"primaryKey" json:"id"`
+	CustomerID uint `json:"customer_id"`
+	Customer   User `gorm:"foreignKey:CustomerID" json:"customer"` // ✅ Tambahkan
+	KurirID    uint `json:"kurir_id"`
+	Kurir      User `gorm:"foreignKey:KurirID" json:"kurir"` // ✅ Tambahkan
+
 	AlamatJemput string         `json:"alamat_jemput"`
 	AlamatAntar  string         `json:"alamat_antar"`
-	NamaBarang   *string        `json:"nama_barang"` // pakai pointer kalau bisa null
+	NamaBarang   *string        `json:"nama_barang"`
 	NamaMakanan  *string        `json:"nama_makanan"`
 	Status       string         `json:"status"`
 	Layanan      string         `json:"layanan"`
