ee03a867ffe5efe7f199c3b3ba5ade5fbf339e34
Ridho Mubarok <dhxo.pyprogram@gmail.com>
2025-08-16 23:04:17 +0700

    merging

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ func main() {
 	// 	AllowCredentials: true,
 	// }))
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000", "http://127.0.0.1:5500", "https://yourapp.web.app"},
+		AllowOrigins:     []string{"*"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
