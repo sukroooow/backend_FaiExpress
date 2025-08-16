9a80f7ea4639a0c498999b71b0f05087fe6a704e
Ridho Mubarok <dhxo.pyprogram@gmail.com>
2025-08-17 04:40:09 +0700

    new

--- config/config.go
+++ config/config.go
@@ -28,8 +28,10 @@ func ConnectDB() {
 	password := os.Getenv("DB_PASSWORD")
 	dbname := os.Getenv("DB_NAME")
 
-	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-		host, port, user, password, dbname)
+	dsn := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=require",
+		host, port, user, password, dbname,
+	)
 
 	var err error
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
